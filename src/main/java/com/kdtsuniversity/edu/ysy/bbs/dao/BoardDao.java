8541dc79afe76a00acd3481e2ffdfc89a4045eaf
seyoung927 <se___young@naver.com>
2025-03-31 06:19:11 +0900

    Reset for folder upload

--- src/main/java/com/kdtsuniversity/edu/ysy/bbs/dao/BoardDao.java
+++
@@ -1,5 +0,0 @@
-package com.kdtsuniversity.edu.ysy.bbs.dao;
-
-public interface BoardDao {
-    String getExample();
-}
--- src/main/java/com/kdtsuniversity/edu/ysy/bbs/vo/BoardListVO.java
+++
@@ -1,8 +0,0 @@
-package com.kdtsuniversity.edu.ysy.bbs.vo;
-
-import java.util.List;
-
-public class BoardListVO {
-    private List<BoardVO> boardList;
-    // getter/setter
-}
