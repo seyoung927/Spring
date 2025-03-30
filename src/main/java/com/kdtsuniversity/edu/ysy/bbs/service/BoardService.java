c628adad86b21f27a8bd028a917eb880826a662d
seyoung927 <se___young@naver.com>
2025-03-31 06:18:21 +0900

    Remove all previously pushed files

--- src/main/java/com/kdtsuniversity/edu/ysy/bbs/service/BoardService.java
+++
@@ -1,5 +0,0 @@
-package com.kdtsuniversity.edu.ysy.bbs.service;
-
-public interface BoardService {
-    String getExampleService();
-}
--- src/main/webapp/WEB-INF/views/board/helloboot.jsp
+++
@@ -1,2 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" %>
-<html><body><h1>Hello, YSY!</h1></body></html>
